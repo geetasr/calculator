453eb6ec255d5b38ec7108e3f7cd819cf31649af
unknown <geeta.s.rathodi@gmail.com>
2023-06-03 11:37:28 +0530

    first commit

---
+++ src/com/Calculator.java
@@ -0,0 +1,36 @@
+package com;
+
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		char operator;
+		double num1,num2,result;
+		Scanner input = new Scanner(System.in);
+		System.out.println("choose the operator: +,-,/");
+		operator = input.next().charAt(0);
+	System.out.println("enter the first number");
+	num1 = input.nextDouble();
+	System.out.println("enter the second number");
+	num2 = input.nextDouble();
+	switch (operator) {
+	case '+' :
+	result = num1 + num2;
+	System.out.println(num1 + " - " + num2 + " = " +result);
+	break;
+	case '-' :
+		result = num1 - num2;
+		System.out.println(num1 + " - " + num2 + " = " +result);
+		break;
+	case '/' :
+		result = num1 / num2;
+		System.out.println(num1 + "/" + num2 + " = " +result);
+		break;
+		default :
+			System.out.println("invalid operator");
+			break;
+	}
+	input.close();
+	}
+
+}
